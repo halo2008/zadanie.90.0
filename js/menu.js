4c1eba36dcf0ed83661169ab065ce850c71d5728
Konrad Sedkowski <ksedkowski@gmail.com>
2018-08-15 10:39:27 +0200

    add working chart

--- js/menu.js
+++ js/menu.js
@@ -36,9 +36,36 @@ function addNew() {
 }
 
 
+function charts() {
+  var ctx = document.getElementById('myChart').getContext('2d');
+  var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+    labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+    datasets: [{
+        label: 'Signups',
+        backgroundColor: '#8DBEC8',
+        borderColor: '#8DBEC8',
+        data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88],
+      }, 
+      {
+        label: "FTD",
+        backgroundColor: '#F29E4E',
+        borderColor: '#F29E4E',
+        data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
+      }, 
+      {
+        label: "Earned",
+        backgroundColor: '#71B374',
+        borderColor: '#71B374',
+        data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
+        hidden: true,          
+      }]    
+    },
+  });
+}
 
-
-
+charts();
 
 
 
@@ -56,9 +83,11 @@ function load_general (e) {
    var con = document.getElementById('content'),
        xhr = new XMLHttpRequest();
 
-   xhr.onreadystatechange = function (e) { 
+   xhr.onload = function (e) { 
     if (xhr.readyState == 4 && xhr.status == 200) {
      con.innerHTML = xhr.responseText;
+
+      charts();
     }
    }
 
@@ -111,10 +140,10 @@ function load_addlink (e) {
      con.innerHTML = xhr.responseText;
     
 /*-----------------modal----------------*/
-      function closeModal() {
-    document.getElementById('overlay').classList.remove('show')
+function closeModal() {
+      document.getElementById('overlay').classList.remove('show')
     }
-    document.querySelectorAll('overlay .js--close-modal').forEach(function(btn) {
+    document.querySelectorAll('#overlay .js--close-modal').forEach(function(btn) {
       btn.addEventListener('click', function(e) {
         e.preventDefault()
         closeModal()
@@ -140,6 +169,7 @@ function load_addlink (e) {
       document.querySelector('#overlay').classList.add('show')
       document.querySelector(modal).classList.add('show')
     }
+    
 /*-----------------------modal--------------------------------*/
     }
    }
@@ -152,38 +182,3 @@ function load_addlink (e) {
 
 
 /*-------------------------Canvas-----------------------*/
-
-var ctx = document.getElementById('myChart').getContext('2D');  
-
-  new Chart(ctx, {
-      // 1
-      type: 'bar',
-      data: {
-          // 2
-          labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
-          // 3
-          datasets: [{
-              // 4
-              label: "Signups",
-              // 5
-              backgroundColor: '#8DBEC8',
-              borderColor: '#8DBEC8',
-              // 6
-              data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
-          },
-          {
-              label: "FTD",
-              backgroundColor: '#F29E4E',
-              borderColor: '#F29E4E',
-              data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
-          },
-          {
-              label: "Earned",
-              backgroundColor: '#71B374',
-              borderColor: '#71B374',
-              data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
-              // 7
-              hidden: true,
-          }]
-      },
-  });
